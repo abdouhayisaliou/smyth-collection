186b9c54d759b6096a9cc8ff9f860e44619ba59c
SALIOU <abdouhayisaliou@gmail.com>
2026-07-28 08:57:42 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -616,6 +616,7 @@ const changeProductImage = (
 
           <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
             {products.map((product, index) => {
+            const currentImage = productSlides[product.id] || 0;
               return (
                 <Reveal
               key={product.id}
