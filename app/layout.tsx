bab9fb4118fb35fb5cb16616784c252a19b768e2
SALIOU <abdouhayisaliou@gmail.com>
2026-09-20 11:10:55 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Smyth Collection",
-
   description: "Luxury Fashion",
 
   manifest: "/manifest.webmanifest",
@@ -15,9 +14,26 @@ export const metadata = {
   },
 
   icons: {
-    icon: "/favicon.ico",
+    icon: [
+      {
+        url: "/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
 
-    apple: "/icons/apple-touch-icon.png",
+    apple: [
+      {
+        url: "/icon-180.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
   },
 };
 
